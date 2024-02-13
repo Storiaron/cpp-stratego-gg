fadc0eb35e45f4724ef4e2e71c653b5f8c8070e9
rBabett <richterbabett03@gmail.com>
2024-02-13 14:26:13 +0100

    add ShopPanel

---
+++ Logic/ShopPanel.h
@@ -0,0 +1,21 @@
+//
+// Created by AsusTuf on 2024. 02. 13..
+//
+
+#pragma once
+
+
+#include "InfoPanel.h"
+
+class ShopPanel : public InfoPanel {
+public:
+    explicit ShopPanel(std::shared_ptr<GameLogic> gameLogic) : InfoPanel(gameLogic){
+        initializeBuyableFigures();
+    };
+    void buyFigure();
+
+private:
+    void initializeBuyableFigures();
+    std::shared_ptr<Figure> selectedFigure;
+    std::shared_ptr<Figure> buyableFigures{new Figure[13]};
+};
