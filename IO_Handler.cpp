15bd468ebb9ca24564b3af24023515a3d5ed2271
StrangerDeer <113778183+StrangerDeer@users.noreply.github.com>
2024-02-13 10:15:55 +0100

    init window and renderer

--- IO_Handler.cpp
+++ IO_Handler.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "IO_Handler.h"
+
+void IO_Handler::initSDL() {
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_CreateWindow("Dradegon", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth,windowHeight,
+                     SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_OPENGL);
+
+    SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+
+}
+
+void IO_Handler::run() {
+
+    while(isRunning){
+        handleEvent();
+        SDL_Delay(25);
+    }
+}
+
+void IO_Handler::handleEvent() {
+
+    while (SDL_PollEvent(&event)){
+        if(event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE){
+            isRunning = false;
+        }
+    }
+}
+
--- IO_Handler.h
+++ IO_Handler.h
@@ -3,8 +3,32 @@
 //
 
 #pragma once
+#include <SDL.h>
 
 
 class IO_Handler {
+public:
+    IO_Handler(){
+        initSDL();
+    }
 
+    ~IO_Handler(){
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+    }
+
+    void run();
+
+private:
+    SDL_Window* window{nullptr};
+    SDL_Renderer* renderer{nullptr};
+    SDL_Event event{};
+
+    const static int windowHeight{1000};
+    const static int windowWidth{1000};
+
+    bool isRunning{true};
+
+    void initSDL();
+    void handleEvent();
 };
