c027d3212469d574aee7352cfcc41438887ee31f
Aaron Storozinszki <115890722+Storiaron@users.noreply.github.com>
2024-02-14 14:20:00 +0100

    switch minus and plus

--- Figures/Soldiers/Paladin.cpp
+++ Figures/Soldiers/Paladin.cpp
@@ -5,10 +5,10 @@
 #include "Paladin.h"
 void Paladin::attack(const std::shared_ptr<Figure> &target) {
   if(target->getPlayerColor() == color) {
-    damage = -7;
+    damage = 7;
   }
   else {
-    damage = 10;
+    damage = -10;
   }
   target->defend(shared_from_this());
 }
