cdfae8ded7926d694feca0f5b0241f92894b31b3
StrangerDeer <113778183+StrangerDeer@users.noreply.github.com>
2024-02-14 11:52:32 +0100

    handle mouse events and can display board

--- IO_Handler.cpp
+++ IO_Handler.cpp
@@ -8,17 +8,59 @@ void IO_Handler::initSDL() {
 
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_CreateWindow("Dradegon", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth,windowHeight,
+    window = SDL_CreateWindow("Dradegon", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth,windowHeight,
                      SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_OPENGL);
 
-    SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+    if (!window) {
+        std::cerr << "Error: Unable to create window!" << SDL_GetError() << std::endl;
+        return;
+    }
 
+    renderer = SDL_CreateRenderer(window, -1,SDL_RENDERER_ACCELERATED);
+
+    if (!renderer) {
+        std::cerr << "Error: Unable to create renderer!" << SDL_GetError() << std::endl;
+        return;
+    }
+
+}
+
+
+void IO_Handler::initLogic() {
+    for(int i = 0; i < numberOfCells; i++){
+        cells.push_back(std::make_shared<Cell>(i));
+    }
+}
+
+
+void IO_Handler::initUI() {
+    int boardWidth = 1010;
+    int boardHeight = 1010;
+    boardUI = std::make_shared<BoardUI>(renderer, boardWidth,boardHeight);
+
+    int cellWidth = 90;
+    int cellHeight = 90;
+    int cellBorderSize = 10;
+    int x = cellBorderSize;
+    int y = cellBorderSize;
+
+    for(int i = 0; i < numberOfCells; i++){
+        boardCellsUI.push_back(std::make_shared<CellUI>(renderer,cells[i], cellWidth, cellHeight, x, y));
+
+        if( x + cellWidth + cellBorderSize >= boardWidth){
+            y = y + cellHeight + cellBorderSize;
+            x = cellBorderSize;
+        } else {
+            x = x + cellWidth + cellBorderSize;
+        }
+    }
 }
 
 void IO_Handler::run() {
 
     while(isRunning){
         handleEvent();
+        display();
         SDL_Delay(25);
     }
 }
@@ -29,6 +71,60 @@ void IO_Handler::handleEvent() {
         if(event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE){
             isRunning = false;
         }
+        if(event.button.type == SDL_MOUSEBUTTONDOWN){
+            handleClick(event.button);
+        }
+        if(event.type == SDL_MOUSEMOTION){
+            handleHover(event.motion);
+        }
     }
 }
+
+void IO_Handler::handleClick(const SDL_MouseButtonEvent &click) {
+    SDL_Point point{click.x, click.y};
+    int index = 0;
+    for (auto &cell : boardCellsUI) {
+        if (SDL_PointInRect(&point, &cell->cellRect) == SDL_TRUE){
+            if(currentCellIndex >= 0){
+                cells[currentCellIndex]->isClicked = false;
+            }
+            currentCellIndex = index;
+            cells[currentCellIndex]->isClicked = true;
+            break;
+        }
+        index++;
+    }
+}
+
+
+
+void IO_Handler::handleHover(SDL_MouseMotionEvent motion) {
+    Sint32 mouseX = motion.x;
+    Sint32 mouseY = motion.y;
+
+    int index = 0;
+
+    for (auto &cell : boardCellsUI) {
+        if((cell->cellRect.x < mouseX) && (cell->cellRect.w + cell->cellRect.x > mouseX) &&
+                (cell->cellRect.y < mouseY) && (cell->cellRect.h + cell->cellRect.y > mouseY)){
+            std::cout << std::to_string(index) << std::endl;
+            break;
+        }
+        index++;
+    }
+}
+
+void IO_Handler::display() {
+    SDL_RenderClear(renderer);
+
+    boardUI->print(renderer);
+
+    for(const std::shared_ptr<CellUI>& cell : boardCellsUI){
+        cell->print(renderer);
+    }
+
+    SDL_RenderPresent(renderer);
+}
+
+
 
--- IO_Handler.h
+++ IO_Handler.h
@@ -4,12 +4,19 @@
 
 #pragma once
 #include <SDL.h>
+#include <memory>
+#include <vector>
+#include "UI/BoardUI.h"
+#include "UI/CellUI.h"
+#include "Logic/Cell.h"
 
 
 class IO_Handler {
 public:
     IO_Handler(){
         initSDL();
+        initLogic();
+        initUI();
     }
 
     ~IO_Handler(){
@@ -24,11 +31,23 @@ private:
     SDL_Renderer* renderer{nullptr};
     SDL_Event event{};
 
-    const static int windowHeight{1000};
-    const static int windowWidth{1000};
+    const static int windowHeight{1010};
+    const static int windowWidth{1010};
+
+    int currentCellIndex{-1};
+    std::vector<std::shared_ptr<Cell>> cells{};
+    std::vector<std::shared_ptr<CellUI>> boardCellsUI;
+    std::shared_ptr<BoardUI> boardUI{nullptr};
 
     bool isRunning{true};
+    int numberOfCells = 100;
 
     void initSDL();
+    void initLogic();
+    void initUI();
+    void display();
     void handleEvent();
+    void handleClick(const SDL_MouseButtonEvent &click);
+    void handleHover(SDL_MouseMotionEvent motion);
+
 };
