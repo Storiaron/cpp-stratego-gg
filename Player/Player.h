790935d1079992dd162660653eff312293a51067
rBabett <richterbabett03@gmail.com>
2024-02-14 13:41:34 +0100

    add isReady to Player and a getter+setter

--- Player/Player.h
+++ Player/Player.h
@@ -13,10 +13,13 @@ public:
     void spendGold(int amount);
     bool canGoldBeSpent(int amount) const;
     void addFigure(const Figure& figure);
+    void setReady();
+    bool getIsReady();
     PlayerColor getColor();
 
 private:
     std::vector<Figure> figures;
     PlayerColor color;
+    bool isReady{false};
     int gold = 350;
 };
