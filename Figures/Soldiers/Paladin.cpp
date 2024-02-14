cb23064c6f9a4b2d0d1c89da895e66148c3b1680
Aaron Storozinszki <115890722+Storiaron@users.noreply.github.com>
2024-02-14 14:18:18 +0100

    Implement paladin switching between friendly-enemy "attacks"

--- Figures/Soldiers/Paladin.cpp
+++ Figures/Soldiers/Paladin.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Paladin.h"
+void Paladin::attack(const std::shared_ptr<Figure> &target) {
+  if(target->getPlayerColor() == color) {
+    damage = -7;
+  }
+  else {
+    damage = 10;
+  }
+  target->defend(shared_from_this());
+}
