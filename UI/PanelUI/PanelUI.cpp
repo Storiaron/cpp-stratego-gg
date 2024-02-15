8cc88e6b2d4f883cc4e9e4f0ebe04f4fe6ca3325
StrangerDeer <113778183+StrangerDeer@users.noreply.github.com>
2024-02-15 14:13:35 +0100

    add figureHP and figureColor to panel

--- UI/PanelUI/PanelUI.cpp
+++ UI/PanelUI/PanelUI.cpp
@@ -2,13 +2,23 @@
 #include "PanelUI.h"
 
 void PanelUI::loadStats(SDL_Renderer *renderer, const std::shared_ptr<InfoPanel>& infoPanel) {
-    int size = 40;
+    int nameSize = 40;
+    int size = 25;
     SDL_Color color = {0,0,0,255};
     if (infoPanel->getCurrentFigureInfo() != nullptr) {
         std::string figureName = infoPanel->getCurrentFigureInfo()->name;
-        std::shared_ptr<GameText> name = std::make_shared<GameText>(renderer, fontPath, size, figureName, color, 10,
+        std::shared_ptr<GameText> name = std::make_shared<GameText>(renderer, fontPath, nameSize, figureName, color, 10,
                                                                     10);
+        std::string figureHp = infoPanel->getCurrentFigureInfo()->currentHp;
+        std::shared_ptr<GameText> hp = std::make_shared<GameText>(renderer, fontPath, size, "Current Hp: " + figureHp, color, 10,
+                                                                  60);
+        std::string figurePlayer = infoPanel->getCurrentFigureInfo()->color;
+        std::shared_ptr<GameText> player = std::make_shared<GameText>(renderer, fontPath, size, "Player: " + figurePlayer, color, 10,
+                                                                  85);
+
         stats.push_back(name);
+        stats.push_back(hp);
+        stats.push_back(player);
     } else {
         stats.clear();
     }
